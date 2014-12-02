568dd13396db07d4e618ee8950f6b13101841beb
harrygfox <henry.george.fox@gmail.com>
2014-12-02 10:18:23 +0000

    Early steps, notes in notes.txt

---
+++ controllers.js
@@ -0,0 +1,13 @@
+app = angular.module('practice', []);
+
+app.controller('firstCtrl', function($scope) {
+    // $scope.data = {
+    //     message: "hello"
+    // };
+});
+
+app.controller('secondCtrl', function($scope) {
+    // $scope.data = {
+    //     message: "goodbye"
+    // };
+});
